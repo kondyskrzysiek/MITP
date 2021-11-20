bd7a4366c9856e99552961324d1cb1388c4b9c23
Krzysiek <krzysiekkondys@gmail.com>
2021-11-20 13:16:10 +0100

    zadania na 24.11.2021

---
+++ zad6.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int numSize(number){
+    int size = 0;
+    while(number>0){
+        size++;
+        number /= 10;
+    }
+
+    return size;
+}
+
+void printNumber(number){
+    int size = 5 - numSize(number);
+    
+    while(size) {
+        printf(" ");
+        size--;
+    }
+
+    printf("%d\n",number);
+}
+
+int main(){
+    int liczba1,liczba2,liczba3,liczba4,wynik;
+
+    printf("Podaj 4 liczby \n");
+    scanf("%d %d %d %d",&liczba1,&liczba2,&liczba3,&liczba4);
+    wynik = liczba1 + liczba2 + liczba3 + liczba4;
+    printNumber(liczba1);
+    printNumber(liczba2);
+    printNumber(liczba3);
+    printNumber(liczba4);
+    printf("-----\n");
+    printNumber(wynik);
+
+
+    return 0;
+}
