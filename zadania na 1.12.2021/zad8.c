edd021fc2c897526191af9939c88c49d2c98e344
Krzysiek <krzysiekkondys@gmail.com>
2021-11-24 13:50:39 +0100

    zadania na 01.12.2021

---
+++ zadania na 1.12.2021/zad8.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Podaj dlugosc boku : ");
+    scanf("%d", &n);
+
+    //wysokosc podstawy gornej
+    for (int i = 0; i < n; i++)
+    {
+        printf("%%");
+    }
+
+    printf("\n");
+
+    //sciany boczne
+    for (int i = 0; i < (n - 2); i++)
+    {
+        printf("%%");
+        for (int j = 0; j < (n - 2); j++)
+        {
+            printf(" ");
+        }
+        printf("%%\n");
+    }
+
+    //wysokosc podstawy dolnej
+    for (int i = 0; i < n; i++)
+    {
+        printf("%%");
+    }
+
+    printf("\n");
+
+    return 0;
+}
