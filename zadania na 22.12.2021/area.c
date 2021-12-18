77658fde1c588106cad62511bc72eb4d257df829
krzychu <krzysiekkondys@gmail.com>
2021-12-18 20:02:58 +0100

    .

--- zadania na 22.12.2021/area.c
+++ zadania na 22.12.2021/area.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include "point.h"
 
+/**
+ * calculating the area of ​​a triangle for 3 points in the coordinate system >> struct POINT __ { int x;  int y; };
+ * 
+ * @param A point A
+ * @param B point B
+ * @param C point C
+ * @return area of ​​a triangle
+ */
 float area(struct POINT A, struct POINT B, struct POINT C)
 {
     float pole;
--- zadania na 22.12.2021/perimeter.c
+++ zadania na 22.12.2021/perimeter.c
@@ -9,6 +9,15 @@ float sideLength(struct POINT A, struct POINT B)
     return lenght;
 }
 
+
+/**
+ * calculating the perimeter of a triangle for 3 points in the coordinate system >> struct POINT __ { int x;  int y; };
+ * 
+ * @param A point A
+ * @param B point B
+ * @param C point C
+ * @return perimeter of ​​a triangle
+ */
 float perimeter(struct POINT A, struct POINT B, struct POINT C)
 {
     return sideLength(A, B) + sideLength(A, C) + sideLength(B, C);
