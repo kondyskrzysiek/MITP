ac25477d661a0b16eb5acd9042f2b0ed83e776dc
krzychu <krzysiekkondys@gmail.com>
2021-12-22 11:28:26 +0100

    prawie finally

--- zad15/zad15.c
+++ zad15/zad15.c
@@ -28,12 +28,8 @@ MATRIX m_create(int x, int y)
 int m_printf(MATRIX tab)
 {
     int size = tab.x * tab.y;
-    // for (int i = 0; i < size; i++)
-    // {
-    //     printf("%d\t", tab.wsk[i]);
-    //     if ((i + 1) % tab.y == 0)
-    //         printf("\n");
-    // }
+    if (tab.x <= 0 || tab.y <= 0 || tab.wsk == NULL)
+        return 0;
 
     for (int x = 0; x < tab.x; x++)
     {
@@ -41,21 +37,24 @@ int m_printf(MATRIX tab)
             printf("%d\t", tab.wsk[x * tab.y + y]);
         printf("\n");
     }
+    printf("\n");
 
     return 1;
 }
 
 int m_get(MATRIX tab, int x, int y)
 {
+    if (x > tab.x || y > tab.y || tab.wsk == NULL)
+        return 0;
     return *(tab.wsk + --x * tab.y + --y);
 }
 
-int m_put(MATRIX tab, int x, int y, int wart)
+int m_put(MATRIX tab, int x, int y, int value)
 {
-    if (x <=0 || y<=0 || x > tab.x || y > tab.y)
+    if (x <= 0 || y <= 0 || x > tab.x || y > tab.y)
         return 0;
 
-    *(tab.wsk + --x * tab.y + --y) = wart;
+    *(tab.wsk + --x * tab.y + --y) = value;
     return 1;
 }
 
@@ -104,31 +103,90 @@ int m_scanf_(MATRIX *mstruct, int x, int y)
         }
     }
 
-    if(!mstruct->wsk)
+    if (!mstruct->wsk)
         return 0;
-    
+
     mstruct->x = x;
     mstruct->y = y;
 
     return m_scanf(mstruct);
 }
 
-
-
 int main()
 {
     MATRIX tab;
-    int a;
-    tab.x = 3;
-    tab.y = 3;
+    int x = 0, y = 0;
+
+    printf("tworzenie macierzy podaj x i y: ");
+    scanf("%d %d", &tab.x, &tab.y);
+
     tab = m_create(tab.x, tab.y);
-    
-    m_printf(tab);
-    
 
+    printf("MACIERZ\n");
+    if (!m_printf(tab))
+    {
+        printf("ERROR : Macierz nie istnieje\n");
+        return 0;
+    }
 
-    if (!m_put(tab,3,3,15))
+    // printf("zapis całej macierzy podaj wiersze i kolumny macierzy <<x y>>\n");
+    // scanf("%d %d", &x, &y);
+
+    // m_scanf_(&tab, x, y);
+
+    // printf("MACIERZ\n");
+    // m_printf(tab);
+
+    // printf("Zmiana wartosci dla 3 poszczegolnych komorek, podaj wiersz kolumne i wartosc jaka wpisac << x y wartosc>> \n");
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     int value = 0;
+    //     printf("%d: ", i + 1);
+    //     scanf("%d %d %d", &x, &y, &value);
+    //     if (!m_put(tab, x, y, value))
+    //     {
+    //         printf("ERROR\n");
+    //         return 0;
+    //     }
+    // }
+
+    // printf("MACIERZ\n");
+    // m_printf(tab);
+
+    // printf("Sprawdz 3 dowolne miejsca w macierzy\n");
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     int a;
+    //     printf("%d: ", i + 1);
+    //     scanf("%d %d", &x, &y);
+    //     a = m_get(tab, x, y);
+    //     if (!a)
+    //     {
+    //         printf("ERROR\n");
+    //         return 0;
+    //     }
+    //     printf(">>> %d\n",a);
+    // }
+
+    // printf("MACIERZ\n");
+    // m_printf(tab);
+
+    printf("usuwanie struktury MATRIX\n");
+
+    if (!m_remove(&tab))
+    {
         printf("ERROR\n");
+        return 0;
+    }
+    else
+        printf("CORRECT\n");
+
+    printf("\nMACIERZ\n");
+    if (!m_printf(tab))
+    {
+        printf("ERROR : Macierz nie istnieje\n");
+        return 0;
+    }
 
     return 0;
 }
