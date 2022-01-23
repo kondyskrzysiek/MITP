a86b005d6438203ef1d373f4e2d4a4f3753e5c8c
krzysiekkondys <krzysiekkondys@gmail.com>
2022-01-24 00:58:12 +0100

    bash file

---
+++ zad21/zad18b.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc zad18b.c swap.c bubble_sort.c select_sort.c -o zad18b
--- zad21/zad19b.sh
+++ zad21/zad19b.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-gcc zad18b.c swap.c bubble_sort.c select_sort.c -o zad18b
 gcc zad19b.c swap.c bubble_sort.c select_sort.c -o zad19b
